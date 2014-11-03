9402d92c90c6d1861d6894d00117ad7fb1d72045
Benjamin Tamborine <vtambourine@ya.ru>
2014-11-04 01:52:04 +0300

    Default tokens file.

---
+++ tokens-default.js
@@ -0,0 +1,14 @@
+/**
+ * GitHub OAuth token.
+ *
+ * @see {@link https://developer.github.com/v3/oauth/}
+ * @type {String}
+ */
+export var githubToken = '';
+
+/**
+ * StarTrek OAuth token.
+ *
+ * @type {String}
+ */
+export var startrekToken = '';
